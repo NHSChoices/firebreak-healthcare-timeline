1822389703a6bbb264bb7e1d387f78e69a0d57f7
Johnathon Broodie <johnathon.broodie@nhs.net>
2016-12-06 18:40:08 +0000

    :sparkles: Add chart and consitent styling

---
+++ js/fitnessChart.js
@@ -0,0 +1,34 @@
+var ctx = document.getElementById("fitnessChart");
+var myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [{
+            label: "",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(0,100,183,0.4)",
+            borderColor: "rgba(0,100,183,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(0,100,183,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(254,254,254,1)",
+            pointHoverBorderColor: "rgba(0,100,183,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40],
+            spanGaps: false,
+        }]
+    },
+    options: {
+        legend: {
+            display: false
+        }
+    } 
+});
